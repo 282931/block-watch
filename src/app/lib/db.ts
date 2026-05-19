ea25a1808fa4e89bed9e6e080ff9678d305b71ff
Theo <for4die@sina.com>
2026-05-19 16:13:52 +0800

    login feat

---
+++ src/app/lib/db.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@/prisma';
+import { PrismaPg } from '@prisma/adapter-pg';
+import pg from 'pg';
+
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.PRISMA_DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
---
+++ src/auth.config.ts
@@ -0,0 +1,43 @@
+import type { NextAuthConfig } from 'next-auth';
+
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      const isOnLogin = nextUrl.pathname === '/login';
+
+      if (isLoggedIn) {
+        if (isOnLogin || nextUrl.pathname === '/') {
+          return Response.redirect(new URL('/dashboard', nextUrl));
+        }
+        return true;
+      }
+
+      // 未登录用户只能访问 login 页面
+      if (isOnLogin) {
+        return true;
+      }
+
+      return false; // 其他页面重定向到 login
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.name = user.name;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+        session.user.name = token.name as string;
+      }
+      return session;
+    },
+  },
+  providers: [], // Add providers with an empty array for now
+} satisfies NextAuthConfig;
